9605932387f47f277bbf5c06a0df464efc0c0106
The Man Daffyo Himself <87585374+DaffyToons@users.noreply.github.com>
2024-02-20 08:25:41 -0300

    Update PsychAlphabet.hx

--- source/android/PsychAlphabet.hx
+++ source/android/PsychAlphabet.hx
@@ -389,7 +389,7 @@ class AlphaCharacter extends FlxSprite
 	public function new(x:Float, y:Float, textSize:Float)
 	{
 		super(x, y);
-		var tex = Paths.getSparrowAtlas('androidcontrols/menu/alphabet');
+		var tex = Paths.getSparrowAtlas('androidcontrols/menu/alphabetOld');
 		frames = tex;
 
 		setGraphicSize(Std.int(width * textSize));
